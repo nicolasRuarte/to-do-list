44deac64b80fbab2cd453d2caa6eddd11e4282a9
nicolasRuarte <miqueasnruarte@protonmail.com>
2024-12-08 17:12:55 -0300

    Added functionality to delete tasks

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const args = proccess.argv;
 const jsonDirectory = "./tasks.json";
 const defaultJSONvalue =  { tasksQuantity: 0, tasks: [] } ;
 
+//Testing functions
 function checkAndCreateJSON(){
     if(fs.existsSync(jsonDirectory)){
         return;
@@ -26,11 +27,29 @@ function checkAndCreateJSON(){
     console.log("Archivo JSON creado");
 }
 
-function createTask(description){
-  if (description === "" || description === undefined){
-    console.error("Add a description of your task!");
-    return;
+function checkIdIsValid(id){
+  const isIdInvalid = parseInt(id) === NaN || id === undefined || id < 0 || id > tasksQuantity;
+  if(isIdInvalid){
+    console.log("Error: Invalid id");
+    return false;
+  }else {
+    return true;
   }
+}
+
+function checkStringArgumentIsValid(arg){
+  if (arg === undefined || arg === ""){
+    console.log("Invalid argument");
+    return false;
+  }else {
+    return true;
+  }
+
+}
+
+//Functionality functions
+function createTask(description){
+  if (!checkStringArgumentIsValid(description)) return;
 
   let tasksList = fs.readFileSync(jsonDirectory, { encoding: "utf-8"});
   const tasksListParse = JSON.parse(tasksList);
@@ -52,15 +71,9 @@ function createTask(description){
 }
 
 function updateTask(id, updatedDescription){
-  const tasksList = JSON.parse(fs.readFileSync(jsonDirectory, { encoding: "utf-8" }));
-  const tasksQuantity = tasksList.tasksQuantity;
-  const isInvalidId = parseInt(id) === NaN || id === undefined || id < 0 || id > tasksQuantity;
-  const isDescriptionInvalid = updatedDescription === "" || updatedDescription === undefined;
-  if(isInvalidId || isDescriptionInvalid){
-    console.log("Error: Invalid id or description");
-    return;
-  }
+  if (!checkIdIsValid(id) || !checkStringArgumentIsValid(updatedDescription)) return;
 
+  const tasksList = JSON.parse(fs.readFileSync(jsonDirectory, { encoding: "utf-8" }));
   const datetime = new Date;
   tasksList.tasks[id].description = updatedDescription;
   tasksList.tasks[id].updatedAt = `${datetime.getDate()}/${datetime.getMonth()}/${datetime.getFullYear()}, ${datetime.getHours()}:${datetime.getMinutes()}:${datetime.getSeconds()}`; 
@@ -71,6 +84,8 @@ function updateTask(id, updatedDescription){
 }
 
 function markTask(id, option){
+  if (!checkIdIsValid(id)) return;
+
   const jsonParse = JSON.parse(fs.readFileSync(jsonDirectory, {encoding: "utf-8"}));
   let previousStatus = "";
 
@@ -96,7 +111,23 @@ function markTask(id, option){
   console.log(`Updated status from ${previousStatus} to ${jsonParse.tasks[id].status} from task with ID of ${id}`);
 }
 
-function deleteTask(id){}
+function deleteTask(id){
+  if (!checkIdIsValid){
+    console.log("Please select a valid task ID")
+    return;
+  }
+
+  const json = JSON.parse(fs.readFileSync(jsonDirectory, { encoding: "utf-8"}));
+  json.tasks.splice(id, 1);
+  json.tasksQuantity--;
+
+  for (let i = 0; i < json.tasks.length; i++){
+    json.tasks[i].id = i;
+  } 
+  fs.writeFileSync(jsonDirectory, JSON.stringify(json));
+
+  console.log("Task deleted succesfully");
+}
 
 function listTasks(selector){
   const tasksList = JSON.parse(fs.readFileSync(jsonDirectory, {encoding: "utf-8"})).tasks;
@@ -158,17 +189,20 @@ function readArgumentList(args){
       updateTask(args[3], args[4]);
       break;
     case "delete":
-      console.log("delete")
+      deleteTask(args[3]);
       break;
     case "list":
       listTasks(args[3]);
       break;
     case "mark-in-progress":
       markTask(args[3], "mark-in-progress")
+      break;
     case "mark-done":
       markTask(args[3], "mark-done");
+      break;
     case "mark-todo":
       markTask(args[3], "mark-todo");
+      break;
     default:
       if(args[2] != undefined) console.log(`"${args[2]}" is not a valid method`);
       break;
